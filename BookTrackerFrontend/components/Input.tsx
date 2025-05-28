39add72ff33dbdaf029268fed24c8b7c4c9594e5
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-05-28 09:42:28 -0300

    style: focus visible style on input and select components

--- BookTrackerFrontend/components/Input.tsx
+++ BookTrackerFrontend/components/Input.tsx
@@ -9,7 +9,7 @@ export function Input(props: ComponentProps<'input'>) {
       name={name}
       value={value}
       onChange={onChange}
-      className={`text-sm w-full p-2 rounded-md border border-zinc-200 ${className}`}
+      className={`focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none focus-visible:ring-offset-2 text-sm w-full p-2 rounded-md border border-zinc-200 ${className}`}
       {...rest}
     />
   )
--- BookTrackerFrontend/components/Select.tsx
+++ BookTrackerFrontend/components/Select.tsx
@@ -4,7 +4,7 @@ export function Select(props: ComponentProps<'select'>) {
   const { id, children, name, value, onChange, className = '', ...rest } = props;
 
   return (
-    <select id={id} name={name} value={value} onChange={onChange} className={`text-sm w-full p-2 rounded-md border border-zinc-200 ${className}`} {...rest}>
+    <select id={id} name={name} value={value} onChange={onChange} className={`focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:outline-none focus-visible:ring-offset-2 text-sm w-full p-2 rounded-md border border-zinc-200 ${className}`} {...rest}>
       {children}
     </select>
   ) 
